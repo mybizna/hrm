d17f4f56cb83fca110d139c92b384846a0f1d5a2
Dedan <dedanirungu@gmail.com>
2024-04-12 12:16:55 +0300

    Changes to files: Entities/Department.php

--- Entities/Department.php
+++ Entities/Department.php
@@ -82,7 +82,7 @@ class Department extends BaseModel
         $rights = parent::rights();
 
         $rights['staff'] = ['view' => true];
-        $rights['registered'] = ['view' => true];
+        $rights['registered'] = [];
         $rights['guest'] = [];
 
         return $rights;
